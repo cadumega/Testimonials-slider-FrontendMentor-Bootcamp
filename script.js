43fe45cc0cf81ff5c3b5c3b35551253d9662321b
cadumega <72901445+cadumega@users.noreply.github.com>
2021-04-07 11:54:59 -0300

    style_part1

---
+++ script.js
@@ -0,0 +1,10 @@
+// const prevBtn = document.querySelector ('.prev');
+// const nextBtn = document.querySelector ('.next');
+
+
+// const users = [
+//   {
+//     name: 'Tanya Sinclair',
+//     prof: 'Ux Designer'
+//   }
+// ]
